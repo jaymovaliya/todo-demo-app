01b813e2adbccf0d6973a2b039ffcc6138612459
jaymovaliya <jaymovaliya786@gmail.com>
2024-04-12 15:08:03 +0530

    Added config, controller and module setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        return {
+          type: 'mongodb',
+          url: process.env.DB_CONNECTION,
+          database: 'todo',
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        }
+      }
+    }),
+    TodoModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
